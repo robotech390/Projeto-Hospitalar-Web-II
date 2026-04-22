8db0afc9e9c858523cc6bb9f2de5b65cc77306c8
Marcus Vinicius <marcusvinicius.mt@zhsolution.com.br>
2026-04-22 11:07:42 -0300

    feat: add metodos genericos ao controller pai

--- equipe5/app/Http/Controllers/Controller.php
+++ equipe5/app/Http/Controllers/Controller.php
@@ -4,5 +4,43 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    protected string $model;
+    protected string $resource;
+    protected array $load = [];
+
+    /**
+     * Listagem genérica
+     */
+    public function index()
+    {
+        $query = $this->model::query();
+
+        if (!empty($this->load)) {
+            $query->with($this->load);
+        }
+
+        return $this->resource::collection($query->get());
+    }
+    
+    /**
+     * Detalhes genéricos
+     */
+    public function show($id)
+    {
+        $record = $this->model::query();
+
+        if (!empty($this->load)) {
+            $record->with($this->load);
+        }
+
+        return new $this->resource($record->findOrFail($id));
+    }
+
+    /**
+     * Regras de validação que o filho deve definir
+     */
+    protected function rules(): array
+    {
+        return [];
+    }
 }
