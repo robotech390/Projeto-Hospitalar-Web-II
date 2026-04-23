51a14dc92393fcf59312068ff1dfd265d0d071c8
dhtrgabriell <dhtrgabriell@outlook.com>
2026-04-23 20:05:00 -0300

    Routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Recepcao\AgendamentoController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -19,6 +20,20 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/faturamento/convenio', function () {
+        return Inertia::render('Faturamento/Convenio', ['convenios' => []]);
+    })->name('faturamento.convenio');
+
+
+    Route::prefix('recepcao')->name('recepcao.')->group(function () {
+        Route::get('/agendamento', [AgendamentoController::class, 'index'])->name('agendamento');
+        Route::get('/medicos', [AgendamentoController::class, 'medicos'])->name('medicos');
+        Route::get('/pacientes', [AgendamentoController::class, 'pacientes'])->name('pacientes');
+        Route::get('/disponibilidade', [AgendamentoController::class, 'disponibilidade'])->name('disponibilidade');
+        Route::post('/agendamento', [AgendamentoController::class, 'store'])->name('agendamento.store');
+        Route::delete('/agendamento/{id}', [AgendamentoController::class, 'destroy'])->name('agendamento.destroy');
+    });
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
