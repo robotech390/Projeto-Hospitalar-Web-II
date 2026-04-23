3b9297afdc921868c69ee78f2dc21bcdec4aefc3
Marcus Vinicius <marcusvinicius.mtt@gmail.com>
2026-04-23 20:59:56 -0300

    feat: get em tipo exame controller

--- equipe5/app/Http/Controllers/Api/TipoExameController.php
+++ equipe5/app/Http/Controllers/Api/TipoExameController.php
@@ -12,6 +12,41 @@ class TipoExameController extends Controller
     protected string $model = TipoExame::class;
     protected string $resource = TipoExameResource::class;
 
+    /**
+     * @OA\Get(
+     *     path="/api/tipos-exame",
+     *     summary="Listar todos os tipos de exame",
+     *     tags={"TipoExame"},
+     *     security={{"sanctum":{}}},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Lista de tipos de exame",
+     *         @OA\JsonContent(type="array", @OA\Items(ref="#/components/schemas/TipoExame"))
+     *     )
+     * )
+     */
+    public function index()
+    {
+        return parent::index();
+    }
+
+    /**
+     * @OA\Post(
+     *     path="/api/tipos-exame",
+     *     summary="Criar um novo tipo de exame",
+     *     tags={"TipoExame"},
+     *     security={{"sanctum":{}}},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(ref="#/components/schemas/TipoExame")
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="Tipo de exame criado",
+     *         @OA\JsonContent(ref="#/components/schemas/TipoExame")
+     *     )
+     * )
+     */
     public function store(Request $request)
     {
         $validated = $request->validate($this->rules());
