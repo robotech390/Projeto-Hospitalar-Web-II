c161646496bb4e472eaa402992cabe61812eb5de
mirelasc23 <mirelamsccomach@gmail.com>
2026-04-26 18:38:24 -0300

    insere model e contrller Convenio

---
+++ app/Models/Convenio.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Convenio extends Model
+{
+    /** @use HasFactory<\Database\Factories\ConvenioFactory> */
+    use HasFactory;
+
+    protected $primaryKey = 'id'; 
+    protected $table = 'convenio';
+    protected $fillable = ['nome', 'cnpj', 'telefone', 'email', 'id_endereco', 'data_criacao', 'data_atualizacao'];
+
+}
---
+++ database/factories/ConvenioFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Convenio;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Convenio>
+ */
+class ConvenioFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
