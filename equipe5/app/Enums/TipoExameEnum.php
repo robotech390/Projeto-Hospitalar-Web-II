a2e6260eb56822e05c419ff8d7ec9d4e45d9c4f5
Marcus Vinicius <marcusvinicius.mtt@gmail.com>
2026-04-26 15:42:13 -0300

    feat: add enum raio-x

--- equipe5/app/Enums/TipoExameEnum.php
+++ equipe5/app/Enums/TipoExameEnum.php
@@ -5,6 +5,7 @@ namespace App\Enums;
 enum TipoExameEnum: string
 {
     case SANGUE = 'Sangue';
+    case RAIO_X = 'Raio-X';
     case IMAGEM = 'Imagem';
     case URINA = 'Urina';
     case OUTRO = 'Outro';
