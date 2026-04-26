b2faa2199faeb9c0702e7896205d570c143ba9a2
hrsmnbru <bruna_hribeiro@hotmail.com>
2026-04-26 19:30:04 -0300

    Ajuste para acessar tela

---
+++ resources/js/Components/Faturamento/FaturamentoLayout.jsx
@@ -0,0 +1,9 @@
+export default function FaturamentoLayout({ children }) {
+    return (
+        <div className="min-h-screen bg-gray-100">
+            <div className="max-w-7xl mx-auto py-6 px-4">
+                {children}
+            </div>
+        </div>
+    );
+}
