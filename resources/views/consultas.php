7344e5d64df723b8affcf1b4d81a32cf1f1cc9a2
Amp&rsand <113919662+MuriloSchneiderS@users.noreply.github.com>
2026-05-03 20:45:36 -0300

    Paginas para crud de consulta
    localhost:8000/consultas - > consultas.php (listagem das consultas com botão para excluir e criar nova)
    
    localhost:8000/consultas/form -> consultaForm.php (formulário para criar nova consulta)

---
+++ resources/views/consultas.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Consultas</title>
+</head>
+<body>
+    <!-- lista de consultas com botões para deletar cada uma e botão para criar novas -->
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/consultas', function () {
+    return view('consultas');
+});
+Route::get('/consultas/form', function () {
+    return view('consultaForm');
+});
