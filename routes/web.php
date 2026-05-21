6ab2a2047add92409de6fcfadebb1164e511a034
Amp&rsand <muriloschneider23@gmail.com>
2026-05-21 20:51:12 -0300

    (api ainda não funciona) atualização do readme

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/consultas', [ConsultaController::class, 'index'])->name('consultas.index');
-Route::get('/consultas/form', [ConsultaController::class, 'create'])->name('consultas.form');
-Route::post('/consultas', [ConsultaController::class, 'store'])->name('consultas.store');
-Route::delete('/consultas/{consulta}', [ConsultaController::class, 'destroy'])->name('consultas.destroy');
-Route::get('/consultas/{consulta}', [ConsultaController::class, 'show'])->name('consultas.show');
-Route::get('/consultas/{consulta}/edit', [ConsultaController::class, 'edit'])->name('consultas.edit');
-Route::put('/consultas/{consulta}', [ConsultaController::class, 'update'])->name('consultas.update');
+Route::get('/consultas', [ConsultaController::class, 'index'])->name('consultas.index');//retorna a view com a lista de consultas
+Route::get('/consultas/form', [ConsultaController::class, 'create'])->name('consultas.form');//retorna a view com o formulário para criar uma nova consulta
+Route::post('/consultas', [ConsultaController::class, 'store'])->name('consultas.store');//recebe os dados do formulário e salva a nova consulta no banco de dados
+Route::delete('/consultas/{consulta}', [ConsultaController::class, 'destroy'])->name('consultas.destroy');//recebe o id da consulta a ser deletada e remove do banco de dados
+Route::get('/consultas/{consulta}', [ConsultaController::class, 'show'])->name('consultas.show');//recebe o id da consulta e retorna a view com os detalhes da consulta
+Route::get('/consultas/{consulta}/edit', [ConsultaController::class, 'edit'])->name('consultas.edit');//recebe o id da consulta e retorna a view com o formulário para editar a consulta
+Route::put('/consultas/{consulta}', [ConsultaController::class, 'update'])->name('consultas.update');//recebe os dados do formulário de edição e atualiza a consulta no banco de dados
