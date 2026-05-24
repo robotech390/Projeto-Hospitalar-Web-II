782c456ea94564f3be6efd9875a78c9906b70384
mirelasc23 <mirelamsccomach@gmail.com>
2026-05-24 14:50:12 -0300

    insere swagger e dependencias

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,40 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ * version="1.0.0",
+ * title="Documentação da API",
+ * description="Interface de documentação do projeto (Laravel + JWT)",
+ * @OA\Contact(
+ * email="seu-email@dominio.com"
+ * )
+ * )
+ * * @OA\Server(
+ * url=L5_SWAGGER_CONST_HOST,
+ * description="Servidor de Desenvolvimento Local"
+ * )
+ * * @OA\SecurityScheme(
+ * securityScheme="bearerAuth",
+ * in="header",
+ * name="Authorization",
+ * type="http",
+ * scheme="bearer",
+ * bearerFormat="JWT",
+ * )
+ */
+abstract class Controller
+{
+    // Este arquivo pode ficar vazio ou abstrato. 
+    // O L5-Swagger vai ler as anotações acima perfeitamente.
+}
+?>
+<?php
+
+/*namespace App\Http\Controllers;
+
 abstract class Controller
 {
     //
 }
+*/
