8aa7e2c0528553c2e970af9fb889f8565411aa77
mirelasc23 <mirelamsccomach@gmail.com>
2026-06-02 21:56:37 -0300

    verificar documentação com l5-swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use OpenApi\Attributes as OA;
 
-/*#[OA\Info(
+#[OA\Info(
     version: "1.0.0",
     title: "Minha API Laravel",
     description: "Documentação da API do meu projeto",
@@ -22,31 +22,6 @@ use OpenApi\Attributes as OA;
     type: "http",
     scheme: "bearer"
 )]
-*/
-
-/**
- * @OA\Info(
- * title="Minha API de Convênios",
- * version="1.0.0"
- * )
- */
-
-
-/**
- * @OA\Info(
- *      title="Minha API Documentation",
- *      version="1.0.0",
- *      description="Documentação interativa da minha API Laravel",
- *      @OA\Contact(
- *          email="seu-email@exemplo.com"
- *      )
- * )
- * 
- * @OA\Server(
- *      url=L5_SWAGGER_CONST_HOST,
- *      description="Ambiente de Desenvolvimento"
- * )
- */
 abstract class Controller
 {
     // Este arquivo pode ficar vazio ou abstrato. 
